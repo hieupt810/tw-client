04b6e64cf546e9a379d3c0d1a898f3ca6b07aa5d
Pham Trung Hieu <trunghieupham03@gmail.com>
2025-04-27 11:13:00 +0700

    fix(suspense): search params

--- src/app/(private)/chat/page.tsx
+++ src/app/(private)/chat/page.tsx
@@ -1,4 +1,5 @@
 import { SquarePen } from 'lucide-react';
+import { Suspense } from 'react';
 
 import { Button } from '@/components/ui/button';
 import { LOREM_IPSUM } from '@/constants';
@@ -7,27 +8,29 @@ import ChatPanel from './components/chat-panel';
 
 export default function ChatPage() {
   return (
-    <div id='chat' className='flex h-[calc(100vh-4rem)] w-full grid-cols-4'>
-      <div className='relative hidden max-h-dvh basis-1/4 flex-col gap-2 overflow-x-hidden overflow-y-auto px-2 py-4 shadow-md md:flex'>
-        <div className='absolute top-0 left-0 z-10 flex w-full items-center justify-between gap-2 px-5 py-2.5'>
-          <h3 className='font-semibold tracking-wide'>History</h3>
-          <Button variant='ghost' size='icon'>
-            <SquarePen />
-          </Button>
-        </div>
+    <Suspense>
+      <div id='chat' className='flex h-[calc(100vh-4rem)] w-full grid-cols-4'>
+        <div className='relative hidden max-h-dvh basis-1/4 flex-col gap-2 overflow-x-hidden overflow-y-auto px-2 py-4 shadow-md md:flex'>
+          <div className='absolute top-0 left-0 z-10 flex w-full items-center justify-between gap-2 px-5 py-2.5'>
+            <h3 className='font-semibold tracking-wide'>History</h3>
+            <Button variant='ghost' size='icon'>
+              <SquarePen />
+            </Button>
+          </div>
 
-        <div className='mt-10 flex grow flex-col gap-1'>
-          {Array.from({ length: 20 }, (_, i) => (
-            <button
-              key={i}
-              className='hover:bg-accent text-accent-foreground cursor-pointer truncate rounded-md px-3 py-1.5 text-sm select-none'
-            >
-              {LOREM_IPSUM}
-            </button>
-          ))}
+          <div className='mt-10 flex grow flex-col gap-1'>
+            {Array.from({ length: 20 }, (_, i) => (
+              <button
+                key={i}
+                className='hover:bg-accent text-accent-foreground cursor-pointer truncate rounded-md px-3 py-1.5 text-sm select-none'
+              >
+                {LOREM_IPSUM}
+              </button>
+            ))}
+          </div>
         </div>
+        <ChatPanel />
       </div>
-      <ChatPanel />
-    </div>
+    </Suspense>
   );
 }
