9cecc77ef1c12bfc18ccd9fa9c2a0d691e9f8206
Pham Trung Hieu <trunghieupham03@gmail.com>
2025-05-04 22:11:01 +0700

    fix: production

--- src/app/(private)/account/components/profile.tsx
+++ src/app/(private)/account/components/profile.tsx
@@ -20,8 +20,7 @@ export default function ProfileCard() {
 
   useEffect(() => {
     getProfile();
-    console.log('getProfile');
-  }, []);
+  }, [getProfile]);
 
   useEffect(() => {
     if (error) {
--- src/app/(private)/chat/page.tsx
+++ src/app/(private)/chat/page.tsx
@@ -2,7 +2,7 @@
 
 import { Loader2, SquarePen } from 'lucide-react';
 import { useRouter, useSearchParams } from 'next/navigation';
-import { Suspense, useEffect } from 'react';
+import { useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { useStore } from 'zustand';
 
@@ -47,59 +47,57 @@ export default function ChatPage() {
     return () => {
       reset();
     };
-  }, [getChatHistory]);
+  }, [getChatHistory, chatHistory.length, reset]);
 
   return (
-    <Suspense>
-      <MaxWidthContainer
-        id='chat'
-        className='grid h-[calc(100svh-7.25rem)] w-full grid-cols-4 p-0'
-      >
-        <div className='border-grid relative col-span-1 hidden max-h-dvh w-full flex-col gap-2 overflow-x-hidden overflow-y-auto border-r md:flex'>
-          <div className='border-grid absolute top-0 left-0 z-10 flex w-full items-center justify-between gap-2 border-b px-4 py-2.5'>
-            <h3 className='ml-2 text-lg font-semibold tracking-tight select-none'>
-              History
-            </h3>
-            <Button
-              size='icon'
-              variant='ghost'
-              aria-label='New chat'
-              onClick={handleNewChat}
-            >
-              <SquarePen />
-            </Button>
-          </div>
-
-          <div className='mt-16 flex grow flex-col gap-1 px-3'>
-            {isLoadingChatHistory && (
-              <div className='flex grow items-center justify-center'>
-                <Loader2 size={36} className='text-primary animate-spin' />
-              </div>
-            )}
-
-            {!isLoadingChatHistory &&
-              chatHistory.length > 0 &&
-              chatHistory.map((item) => (
-                <button
-                  key={item.id}
-                  aria-label={`Open chat ${item.id}`}
-                  onClick={() => router.replace(`/chat?id=${item.id}`)}
-                  className={cn(
-                    'hover:bg-accent text-accent-foreground cursor-pointer truncate rounded-md px-3 py-1.5 text-justify text-sm tracking-tight select-none',
-                    item.id === chatId && 'text-primary font-semibold',
-                  )}
-                >
-                  Conversation {formatDate(item.created_at)}
-                </button>
-              ))}
-
-            {!isLoadingChatHistory && chatHistory.length === 0 && (
-              <p className='py-5 text-center'>No recent chat</p>
-            )}
-          </div>
+    <MaxWidthContainer
+      id='chat'
+      className='grid h-[calc(100svh-7.25rem)] w-full grid-cols-4 p-0'
+    >
+      <div className='border-grid relative col-span-1 hidden max-h-dvh w-full flex-col gap-2 overflow-x-hidden overflow-y-auto border-r md:flex'>
+        <div className='border-grid absolute top-0 left-0 z-10 flex w-full items-center justify-between gap-2 border-b px-4 py-2.5'>
+          <h3 className='ml-2 text-lg font-semibold tracking-tight select-none'>
+            History
+          </h3>
+          <Button
+            size='icon'
+            variant='ghost'
+            aria-label='New chat'
+            onClick={handleNewChat}
+          >
+            <SquarePen />
+          </Button>
         </div>
-        <ChatPanel />
-      </MaxWidthContainer>
-    </Suspense>
+
+        <div className='mt-16 flex grow flex-col gap-1 px-3'>
+          {isLoadingChatHistory && (
+            <div className='flex grow items-center justify-center'>
+              <Loader2 size={36} className='text-primary animate-spin' />
+            </div>
+          )}
+
+          {!isLoadingChatHistory &&
+            chatHistory.length > 0 &&
+            chatHistory.map((item) => (
+              <button
+                key={item.id}
+                aria-label={`Open chat ${item.id}`}
+                onClick={() => router.replace(`/chat?id=${item.id}`)}
+                className={cn(
+                  'hover:bg-accent text-accent-foreground cursor-pointer truncate rounded-md px-3 py-1.5 text-justify text-sm tracking-tight select-none',
+                  item.id === chatId && 'text-primary font-semibold',
+                )}
+              >
+                Conversation {formatDate(item.created_at)}
+              </button>
+            ))}
+
+          {!isLoadingChatHistory && chatHistory.length === 0 && (
+            <p className='py-5 text-center'>No recent chat</p>
+          )}
+        </div>
+      </div>
+      <ChatPanel />
+    </MaxWidthContainer>
   );
 }
