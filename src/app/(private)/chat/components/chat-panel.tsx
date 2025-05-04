6bb7bda5551f537cdf2c4cb7b665e6bf71f3b6d1
Pham Trung Hieu <trunghieupham03@gmail.com>
2025-05-04 22:06:01 +0700

    fix: production

--- src/app/(private)/chat/components/chat-panel.tsx
+++ src/app/(private)/chat/components/chat-panel.tsx
@@ -2,7 +2,7 @@
 
 import { Loader2 } from 'lucide-react';
 import { useRouter, useSearchParams } from 'next/navigation';
-import { useEffect, useRef, useState } from 'react';
+import { Suspense, useEffect, useRef, useState } from 'react';
 import { toast } from 'sonner';
 import { v4 as uuidv4 } from 'uuid';
 import { useStore } from 'zustand';
@@ -73,53 +73,55 @@ export default function ChatPanel() {
   }, [error]);
 
   return (
-    <div
-      ref={boxChatRef}
-      className='col-span-3 flex max-h-[calc(100svh-7.25rem)] w-full flex-col'
-    >
-      {isLoadingMessage && (
-        <div className='flex grow items-center justify-center'>
-          <Loader2 size={36} className='text-primary animate-spin' />
+    <Suspense>
+      <div
+        ref={boxChatRef}
+        className='col-span-3 flex max-h-[calc(100svh-7.25rem)] w-full flex-col'
+      >
+        {isLoadingMessage && (
+          <div className='flex grow items-center justify-center'>
+            <Loader2 size={36} className='text-primary animate-spin' />
+          </div>
+        )}
+        {!isLoadingMessage && (
+          <div
+            className={cn(
+              'flex max-h-[calc(100vh-4rem)] grow flex-col-reverse gap-y-4 overflow-y-auto px-5 py-4',
+              messageHistory.length === 0 && 'justify-center',
+            )}
+          >
+            {isLoadingResponse && <Message isLoading />}
+
+            {messageHistory.length > 0 &&
+              messageHistory.map((item, index) => (
+                <Message key={index} text={item.text} isUser={item.is_user} />
+              ))}
+
+            {messageHistory.length === 0 && (
+              <div className='inline-flex items-center justify-center gap-2 text-2xl leading-tight font-semibold tracking-tighter sm:text-3xl md:text-4xl lg:leading-[1.1]'>
+                <span>Hello,</span>
+                {me ? (
+                  <span className='text-primary'>{me.name}</span>
+                ) : (
+                  <Skeleton className='ml-1 h-8 w-48' />
+                )}
+              </div>
+            )}
+          </div>
+        )}
+
+        <div className='px-5 py-3'>
+          <Input
+            type='text'
+            value={text}
+            onKeyDown={onKeyDown}
+            onChange={onInputChange}
+            aria-label='Type your message'
+            placeholder='Ask something...'
+            disabled={isLoadingMessage || isLoadingResponse}
+          />
         </div>
-      )}
-      {!isLoadingMessage && (
-        <div
-          className={cn(
-            'flex max-h-[calc(100vh-4rem)] grow flex-col-reverse gap-y-4 overflow-y-auto px-5 py-4',
-            messageHistory.length === 0 && 'justify-center',
-          )}
-        >
-          {isLoadingResponse && <Message isLoading />}
-
-          {messageHistory.length > 0 &&
-            messageHistory.map((item, index) => (
-              <Message key={index} text={item.text} isUser={item.is_user} />
-            ))}
-
-          {messageHistory.length === 0 && (
-            <div className='inline-flex items-center justify-center gap-2 text-2xl leading-tight font-semibold tracking-tighter sm:text-3xl md:text-4xl lg:leading-[1.1]'>
-              <span>Hello,</span>
-              {me ? (
-                <span className='text-primary'>{me.name}</span>
-              ) : (
-                <Skeleton className='ml-1 h-8 w-48' />
-              )}
-            </div>
-          )}
-        </div>
-      )}
-
-      <div className='px-5 py-3'>
-        <Input
-          type='text'
-          value={text}
-          onKeyDown={onKeyDown}
-          onChange={onInputChange}
-          aria-label='Type your message'
-          placeholder='Ask something...'
-          disabled={isLoadingMessage || isLoadingResponse}
-        />
       </div>
-    </div>
+    </Suspense>
   );
 }
