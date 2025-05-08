fc1c2d37dda41a6bc6fffb6043832fd6cacb5886
Pham Trung Hieu <trunghieupham03@gmail.com>
2025-05-08 22:35:10 +0700

    chore: border footer

--- src/components/app-footer.tsx
+++ src/components/app-footer.tsx
@@ -1,9 +1,13 @@
+import MaxWidthContainer from './max-width-container';
+
 export default function AppFooter() {
   return (
     <footer className='border-grid flex h-14 items-center justify-center border-t'>
-      <p className='text-muted-foreground text-center text-base text-balance'>
-        &copy; {new Date().getFullYear()} TripWise. All rights reserved.
-      </p>
+      <MaxWidthContainer className='border-x'>
+        <p className='text-muted-foreground text-center text-base text-balance'>
+          &copy; {new Date().getFullYear()} TripWise. All rights reserved.
+        </p>
+      </MaxWidthContainer>
     </footer>
   );
 }
