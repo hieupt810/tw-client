5c01d48e130cd755a073b4644ec0b84beb06996d
Pham Trung Hieu <trunghieupham03@gmail.com>
2025-05-10 20:32:19 +0700

    fix: constants

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,7 @@
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
-import { ACCESS_TOKEN_KEY, REFRESH_TOKEN_KEY } from '@/constants';
+import { Constant } from '@/constants';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -9,9 +9,12 @@ export function cn(...inputs: ClassValue[]) {
 
 export function getTokenPair() {
   if (typeof window === 'undefined') return null;
-  const accessToken = localStorage.getItem(ACCESS_TOKEN_KEY);
-  const refreshToken = localStorage.getItem(REFRESH_TOKEN_KEY);
-
+  const accessToken = localStorage.getItem(
+    Constant.LOCAL_STORAGE_KEY.ACCESS_TOKEN_KEY,
+  );
+  const refreshToken = localStorage.getItem(
+    Constant.LOCAL_STORAGE_KEY.REFRESH_TOKEN_KEY,
+  );
   if (!accessToken || !refreshToken) return null;
   return { accessToken, refreshToken };
 }
