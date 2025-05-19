d78744de70aec2d514b2b56b724066829191b8ed
Pham Trung Hieu <trunghieupham03@gmail.com>
2025-05-19 09:16:21 +0700

    fix(ux): private layout

--- src/app/(public)/(private)/layout.tsx
+++ src/app/(public)/(private)/layout.tsx
@@ -1,8 +1,11 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
+import { toast } from 'sonner';
 import { useStore } from 'zustand';
 
+import Loading from '@/components/loading';
 import { useAuthStore } from '@/stores/auth';
 
 export default function PrivateLayout({
@@ -10,13 +13,16 @@ export default function PrivateLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const router = useRouter();
   const { me, isLoading } = useStore(useAuthStore, (state) => state);
 
   useEffect(() => {
     if (!me && !isLoading) {
-      window.location.href = '/sign-in';
+      router.push('/sign-in');
+      toast.error('You need to sign in to access this page');
     }
   }, [me, isLoading]);
 
+  if (!me) return <Loading />;
   return children;
 }
